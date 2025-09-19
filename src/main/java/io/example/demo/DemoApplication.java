0a94b3a668f99bd5b49cbde66a46bfb08e3d3d88
cwroble <63411279+cwroblecb@users.noreply.github.com>
2025-09-19 11:31:20 -0500

    Create DemoApplication.java

---
+++ src/main/java/io/example/demo/DemoApplication.java
@@ -0,0 +1,11 @@
+package io.example.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+  @GetMapping("/")
+  public String hello() { return "Hello from Harness!"; }
+  public static void main(String[] args) { SpringApplication.run(DemoApplication.class, args); }
+}
