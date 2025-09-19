3acad312d01ba49ce8fad2c4e2b8fb7c267278c5
cwroble <63411279+cwroblecb@users.noreply.github.com>
2025-09-19 11:32:44 -0500

    Create DemoTest.java

---
+++ src/main/java/io/example/demo/DemoTest.java
@@ -0,0 +1,6 @@
+package io.example.demo;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+public class DemoTest {
+  @Test void ok(){ assertTrue(1+1==2); }
+}
